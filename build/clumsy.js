3a84427ea51e9e062b84211ec98cefb0a7c1d401
PanMan <martijn@panman.nl>
2014-06-06 15:19:38 +0200

    debug

--- build/clumsy.js
+++ build/clumsy.js
@@ -1,6 +1,6 @@
 
 //me.sys.fps = 30;
-
+me.debug.displayFPS = true; 
 var game = {
   data: {
     score : 0,
@@ -23,7 +23,7 @@ var game = {
 
   "loaded": function() {
     //console.log('Loaded');
-    
+    me.plugin.register(debugPanel, "debug"); //OWN
     me.state.set(me.state.MENU, new game.TitleScreen());
     me.state.set(me.state.PLAY, new game.PlayScreen());
     me.state.set(me.state.GAME_OVER, new game.GameOverScreen());
