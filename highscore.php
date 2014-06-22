5ff3d93f42033605b99dbd48b6e848fca4b49d05
PanMan <martijn@panman.nl>
2014-06-22 14:55:33 +0200

    highscore stuff

--- highscore.php
+++ highscore.php
@@ -5,3 +5,4 @@ $prevscore=file_get_contents("highscore.txt");
 if ($score>$prevscore){
 	file_put_contents("highscore.txt", $score);
 }
+file_put_contents("highscores.txt", $score."\n", FILE_APPEND);
