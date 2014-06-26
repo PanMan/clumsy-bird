dbcafa649dc9c7d51f7bfad2251ccc602d176626
PanMan <martijn@panman.nl>
2014-06-26 16:41:04 +0200

    skip 0 and : seperated

--- highscore.php
+++ highscore.php
@@ -1,11 +1,11 @@
 <?php
 $score=intval($_REQUEST['score']);
-
+if ($score==0) {exit;} //Skip 0 scores
 $highscore=file_get_contents("highscore.txt");
 if ($score>$highscore){
 	file_put_contents("highscore.txt", $score);
 	$highscore=$score;
 }
 file_put_contents("highscores.txt", $score."\n", FILE_APPEND);
-$out=$score."\n".$highscore."\n".time();
+$out=$score.":".$highscore.":".time();
 file_put_contents("lastscore.txt", $out);
