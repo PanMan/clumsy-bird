a77a03977ddc4d200b456c10b258ce872c497ae1
PanMan <martijn@panman.nl>
2014-06-26 14:22:53 +0200

    highscore stuff

--- highscore.php
+++ highscore.php
@@ -1,8 +1,11 @@
 <?php
 $score=intval($_REQUEST['score']);
 
-$prevscore=file_get_contents("highscore.txt");
-if ($score>$prevscore){
+$highscore=file_get_contents("highscore.txt");
+if ($score>$highscore){
 	file_put_contents("highscore.txt", $score);
+	$highscore=$score;
 }
 file_put_contents("highscores.txt", $score."\n", FILE_APPEND);
+$out=$score."\n".$highscore."\n".time();
+file_put_contents("lastscore.txt", $out);
